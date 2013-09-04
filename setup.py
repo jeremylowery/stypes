805ec43b1f9c15880eb85034feee7654ebbfc225
Jeremy Lowery <jeremy@server.fake>
2013-09-03 21:48:58 -0500

    added setup classifiers and version information

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
 
 from distutils.core import setup
+from stypes.version import get_version
 
-__version__ = '0.1-dev'
+long_description = open("README.md").read()
 
 setup(
     name='stypes',
-    version=__version__,
+    version=get_version('short'),
+    description='rich type library for textual data records',
+    long_description=long_description,
     provides=['stypes'],
     packages=['stypes'],
     author="Jeremy Lowery",
     author_email="jeremy@bitrel.com",
-    url="http://github.com/jeremylowery/stypes"
+    url="http://github.com/jeremylowery/stypes",
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration'
+    ],
 )
