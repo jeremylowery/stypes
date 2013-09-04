39c3b8d3c3153389dc56b37e12ca7cfb7f743260
Jeremy Lowery <jeremy@server.fake>
2013-09-03 20:03:29 -0500

    fixed package name in setup

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 __version__ = '0.1-dev'
 
 setup(
-    name='textr',
+    name='stypes',
     version=__version__,
-    provides=['textr'],
-    packages=['textr'],
+    provides=['stypes'],
+    packages=['stypes'],
     author="Jeremy Lowery",
     author_email="jeremy@bitrel.com",
-    url="http://github.com/jeremylowery/textr"
+    url="http://github.com/jeremylowery/stypes"
 )
