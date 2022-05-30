ea6820dff0814ad0fb3d8b599a933c6076ac5d54
Jeremy Lowery <jeremy@bitrel.com>
2022-05-30 16:07:32 -0500

    migrated to setuptools

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
-
-from distutils.core import setup
+import setuptools
+#from distutils.core import setup
 from stypes.version import get_version
 
-## broken difference between markdown and rst. These patterns
-## blow up python setup.py register
 long_description = open("README.md").read()
-long_description = long_description.replace('```python', '::')
-long_description = long_description.replace('```bash', '::')
-long_description = long_description.replace('```', '\n')
 
-setup(
+setuptools.setup(
     name='stypes',
     version=get_version('short'),
     description='Encode and Decode Textual Data into Rich Python Data Structures',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     provides=['stypes'],
     packages=['stypes'],
     author="Jeremy Lowery",
